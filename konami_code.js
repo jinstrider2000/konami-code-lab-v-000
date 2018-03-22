3ba9c2ac8d69432335ba0e4a73134b2ab3ed3a77
Efrain Perez Jr <efrainperezjr@live.com>
2018-03-22 20:08:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  var index = 0, lastKey;
+  function konamiCode(e)
+  {
+    lastKey = e.which();
+    if (lastKey == code[index] )
+    {
+      index++;
+      if (index === code.length)
+      {
+        alert("You get 30 Lives!");
+      }
+    }
+    else
+    {
+      index = 0;
+    }
+  }
+  document.body.addEventListener("keydown", konamiCode)
 }
