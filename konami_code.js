3de94e5387b4e3024a16ebbb2e94ab67c037f311
Efrain Perez Jr <efrainperezjr@live.com>
2018-03-22 22:26:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,7 +11,7 @@ function init() {
       index++;
       if (index === code.length)
       {
-        alert("You get 30 Lives!");
+        window.alert("You get 30 Lives!");
         index = 0;
       }
     }
